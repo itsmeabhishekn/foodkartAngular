9199fac0256c31de8c465d9e9d304caf58790926
itsmeabhishekn <abhishekn4454@gmail.com>
2022-11-25 00:07:10 +0530

    added explore food menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SingupComponent } from './singup/singup.component';
+import { ExploreFoodComponent } from './explore-food/explore-food.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     SignInComponent,
-    SingupComponent
+    SingupComponent,
+    ExploreFoodComponent
   ],
   imports: [
     BrowserModule,
